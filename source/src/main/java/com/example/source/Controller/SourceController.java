9d75497038c832b58f5243e037403b6b63faa3d8
jonghwee <jh.park2@okestro.com>
2022-09-06 09:48:03 +0900

    multimodule - 생성

---
+++ source/src/main/java/com/example/source/Controller/SourceController.java
@@ -0,0 +1,43 @@
+package com.example.source.Controller;
+
+import com.example.source.DTO.Member;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+
+/**
+ * Project : MultiModule
+ * Created by OKESTRO
+ * Developer : jonghwee
+ * Date Time : 2022/09/05 20:05
+ * Summary :
+ **/
+@RestController
+//@RequiredArgsConstructor
+public class SourceController {
+    private final WebClient webclient = WebClient.create();
+
+    @GetMapping("/hello")
+    public Mono<String> get(){
+        return webclient.get()
+                .uri("/hello")
+                .retrieve()
+                .bodyToMono(String.class);
+    }
+
+    @PostMapping("/member")
+    public Mono<ResponseEntity<Member>> post(){
+        return webclient.post()
+                .uri("/member")
+                .retrieve()
+                .toEntity(new ParameterizedTypeReference<Member>() {
+                });
+    }
+}
---
+++ source/src/main/java/com/example/source/DTO/Member.java
@@ -0,0 +1,15 @@
+package com.example.source.DTO;
+
+import lombok.Data;
+
+/**
+ * Project : MultiModule
+ * Created by OKESTRO
+ * Developer : jonghwee
+ * Date Time : 2022/09/05 20:16
+ * Summary :
+ **/
+@Data
+public class Member {
+    private Member member;
+}
